daf63f043707213f47a2aba8dd1db8ba81f3f1e6
Lucas <lucasatletico@hotmail.com>
2020-06-03 16:15:13 -0300

    primeiro commit

---
+++ src/test/java/com/example/exercicossb/ExercicosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
