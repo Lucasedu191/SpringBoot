9d02229c74e033f7cabe739e920a95d5664c1bf7
Lucas <lucasatletico@hotmail.com>
2020-06-03 16:45:21 -0300

    criando primeiro web service com get mapping

--- src/main/java/com/example/exercicossb/controllers/PrimeiroController.java
+++ src/main/java/com/example/exercicossb/controllers/PrimeiroController.java
@@ -1,5 +1,18 @@
 package com.example.exercicossb.controllers;
 
-public class PrimeiroController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class PrimeiroController {
+	
+	@GetMapping(path = {"/ola"})
+	public String ola() {
+		return "Olá Spring Boot!";
+	}
+	
+	@GetMapping(path = {"/ola"})
+	public String saudacao() {
+		return "Olá Spring Boot!";
+	}
 }
