f5a0c4cac1d6e54bea5b39b28ee073242a24a7b6
Lucas <lucasatletico@hotmail.com>
2020-06-04 20:20:12 -0300

    Inserindo produto2

--- src/main/java/com/example/exercicossb/model/entities/Produto.java
+++ src/main/java/com/example/exercicossb/model/entities/Produto.java
@@ -4,26 +4,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@NotBlank
 	private String nome;
-	
+	@Min(value = 0)
+	private double preco;
+	@Min(0)
+	@Max(1)
+	private double desconto;
 	public Produto() {
 		
 	}
-	
-	
-	
-	public Produto(String nome) {
+
+	public Produto(String nome, double preco, double desconto) {
 		super();
 		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
 	}
-
-
 
 	public int getId() {
 		return id;
@@ -36,6 +42,22 @@ public class Produto {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
 	}
 	
 	
